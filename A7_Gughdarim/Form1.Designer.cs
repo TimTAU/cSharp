5055999ae26f405948b8ae63f896ded8077cd7b4
tau <tim.augustin@veda.net>
2020-05-29 15:15:04 +0200

    Vorlesung 7

---
+++ A7_Gughdarim/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace A7_Gughdarim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.finishBtn = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.restartBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(24, 12);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(75, 23);
+            this.drawBtn.TabIndex = 0;
+            this.drawBtn.Text = "Ziehen";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.Draw);
+            // 
+            // finishBtn
+            // 
+            this.finishBtn.Location = new System.Drawing.Point(105, 12);
+            this.finishBtn.Name = "finishBtn";
+            this.finishBtn.Size = new System.Drawing.Size(75, 23);
+            this.finishBtn.TabIndex = 1;
+            this.finishBtn.Text = "End";
+            this.finishBtn.UseVisualStyleBackColor = true;
+            this.finishBtn.Click += new System.EventHandler(this.End);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(305, 17);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(229, 64);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(433, 64);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(649, 64);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(24, 270);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(111, 151);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(229, 271);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 9;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(433, 271);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 10;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(649, 271);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 11;
+            this.pictureBox8.TabStop = false;
+            // 
+            // restartBtn
+            // 
+            this.restartBtn.Location = new System.Drawing.Point(186, 12);
+            this.restartBtn.Name = "restartBtn";
+            this.restartBtn.Size = new System.Drawing.Size(75, 23);
+            this.restartBtn.TabIndex = 12;
+            this.restartBtn.Text = "Restart";
+            this.restartBtn.UseVisualStyleBackColor = true;
+            this.restartBtn.Click += new System.EventHandler(this.Restart);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 460);
+            this.Controls.Add(this.restartBtn);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.finishBtn);
+            this.Controls.Add(this.drawBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button drawBtn;
+        private System.Windows.Forms.Button finishBtn;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button restartBtn;
+    }
+}
+
---
+++ A7_Gughdarim/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A7_Gughdarim
+{
+    public partial class Form1 : Form
+    {
+        private const int CardsCols = 13, CardsRows = 4;
+        private readonly Bitmap[,] _cards = new Bitmap[CardsCols, CardsRows];
+        private int _nextCard, _score;
+        private readonly Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // from http://www.ironstarmedia.co.uk/2010/01/free-game-assets-08-playing-card-pack/
+            Bitmap bmpOrig = new Bitmap("../../Cards/classic-cards-gray.gif");
+
+            int szX = bmpOrig.Width / CardsCols;
+            int szY = bmpOrig.Height / CardsRows;
+
+            for (int iy = 0; iy < 4; ++iy)
+            {
+                int y = ((iy & 1) != 0) ? 4 - iy : iy; // Tausche Reihe 1 und 3
+                for (int x = 0; x < 13; ++x)
+                {
+                    Rectangle rect = new Rectangle(x * szX, iy * szY, szX, szY);
+                    _cards[x, y] = Copy(bmpOrig, rect);
+                }
+            }
+
+            Start();
+        }
+
+        private static Bitmap Copy(Bitmap srcBitmap, Rectangle section)
+        {
+            // from MSDN
+            Bitmap bmp = new Bitmap(section.Width, section.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(srcBitmap, 0, 0, section, GraphicsUnit.Pixel);
+            g.Dispose();
+            return bmp;
+        }
+
+        private void Draw(object sender, EventArgs e)
+        {
+            RenderCard(_nextCard);
+        }
+
+        private void Restart(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+        
+        private void End(object sender, EventArgs e)
+        {
+            EndGame();
+        }
+
+        private void Start()
+        {
+            RenderCard(0);
+            RenderCard(1);
+            _nextCard = 2;
+        }
+
+        private void RenderCard(int pos)
+        {
+            int r1 = _random.Next(0, 13);
+            int r2 = _random.Next(0, 3);
+
+            switch (pos)
+            {
+                case 0:
+                    pictureBox1.Image = _cards[r1, r2];
+                    break;
+                case 1:
+                    pictureBox2.Image = _cards[r1, r2];
+                    break;
+                case 2:
+                    pictureBox3.Image = _cards[r1, r2];
+                    break;
+                case 3:
+                    pictureBox4.Image = _cards[r1, r2];
+                    break;
+                case 4:
+                    pictureBox5.Image = _cards[r1, r2];
+                    break;
+                case 5:
+                    pictureBox6.Image = _cards[r1, r2];
+                    break;
+                case 6:
+                    pictureBox7.Image = _cards[r1, r2];
+                    break;
+                case 7:
+                    pictureBox8.Image = _cards[r1, r2];
+                    break;
+                default:
+                    return;
+            }
+
+            switch (r1)
+            {
+                case 0:
+                    _score += 11;
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    _score += 10;
+                    break;
+                default:
+                    _score += r1 + 1;
+                    break;
+            }
+            ValidateScore();
+            _nextCard++;
+        }
+
+        private void ValidateScore()
+        {
+            if (_score >= 21)
+            {
+                EndGame();
+            }
+        }
+
+        private void EndGame()
+        {
+            int opponent = _random.Next(4, 22);
+            if (_score < opponent)
+            {
+                resultLabel.Text = "Looser. Opponent: " + opponent;
+            }
+            else if (_score == opponent)
+            {
+                resultLabel.Text = "Draw. Opponent: " + opponent;
+            }
+            else
+            {
+                resultLabel.Text = "Winner. Opponent: " + opponent;
+            }
+            drawBtn.Enabled = false;
+            finishBtn.Enabled = false;
+        }
+    }
+}
